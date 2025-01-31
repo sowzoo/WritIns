3009fde813fd22032435e2fe50f5462b5c96f209
unknown <joeyk2k@gmail.com>
2025-01-31 20:06:13 +0800

    links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://insurance-forms.vercel.app",
 	integrations: [icon(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
