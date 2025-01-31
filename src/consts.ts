f9064e7a1f82fd81333619382e2e83162104cc9d
Michael Barber <mike@cognito.sg>
2025-01-31 11:57:56 -0500

    updated site name and desription

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Site Title";
-export const SITE_DESCRIPTION = "Very Descritive Title";
+export const SITE_TITLE = "Agent Writing Reference";
+export const SITE_DESCRIPTION = "Agent Tools for Writing Business";
